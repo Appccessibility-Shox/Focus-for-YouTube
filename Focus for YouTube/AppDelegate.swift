c82cce50136919513f47964526dc6c49043c3073
patrickshox <patrick.botros@duke.edu>
2020-05-20 23:23:04 -0400

    Initial Commit

---
+++ Focus for YouTube/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Focus for YouTube
+//
+//  Created by Patrick Botros on 5/20/20.
+//  Copyright © 2020 Patrick Botros. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
